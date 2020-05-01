f1efef17ed860475ca4e10005fee061fad95c3a4
JIAWEI LYU <ljw960926@gmail.com>
2020-04-30 19:53:45 -0500

    add data download script

---
+++ update.sh
@@ -0,0 +1,4 @@
+mkdir -p data
+wget -P ./data https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv
+wget -P ./data https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv
+wget -P ./data https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
