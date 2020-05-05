56b425d9949e19950cc6ff352b3a9bf0b9d668cc
JIAWEI LYU <ljw960926@gmail.com>
2020-05-04 19:01:17 -0500

    add per_capita related

--- analytics/covid19.py
+++ analytics/covid19.py
@@ -1,15 +1,19 @@
 import os
+import requests
 
 import pandas as pd
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), '../data/')
+BASE_URL = 'https://api.census.gov/data/2019/pep/population'
+API_KEY = 'ff51fda390a45a5085af02b99725c2963edf7931'
 
 
 class Covid19(object):
     def __init__(self):
-        self.df_us = pd.read_csv(os.path.join(DATA_DIR, 'us.csv'))
-        self.df_states = pd.read_csv(os.path.join(DATA_DIR, 'us-states.csv')).drop(axis=1, labels=['fips'])
-        self.df_counties = pd.read_csv(os.path.join(DATA_DIR, 'us-counties.csv')).drop(axis=1, labels=['fips'])
+        self.df_us = pd.read_csv(os.path.join(DATA_DIR, 'us.csv'), dtype={'fips': str})
+        self.df_states = pd.read_csv(os.path.join(DATA_DIR, 'us-states.csv'), dtype={'fips': str})
+        self.df_counties = pd.read_csv(os.path.join(DATA_DIR, 'us-counties.csv'), dtype={'fips': str})
+        self.df_counties = self.df_counties[self.df_counties['county'] != 'Unknown']
 
     def __str__(self):
         return 'us: {}\nus-states: {}\nus-counties: {}'.format(
@@ -35,8 +39,7 @@ class Covid19(object):
             df = self.df_counties.copy()
             df = df[(df['state'] == state) & (df['county'] == county)]
         df = df.reset_index(drop=True)
-        idx = pd.Index(df['date']).get_loc(date)
-
+        idx = pd.Index(df['date']).get_loc(date) if df['date'].iloc[-1] <= date else 0
         return df.iloc[idx]['cases'] - df.iloc[idx - span if idx - span > 0 else 0]['cases']
 
     def growth_rate(self, date, state=None, county=None, span=1):
@@ -133,16 +136,20 @@ class Covid19(object):
     """
     Additional Data needed
     """
-    def confirmed_case_per_capita(self, date=None, state=None, county=None, scale='million'):
+    def confirmed_case_per_capita(self, date=None, state=None, county=None, scale=100000, span=999):
         """
         Confirmed case per capita
         :param date: str, e.g. '2020-04-23'
         :param state: str, e.g. 'Illinois'
-        :param county: 'new' or 'cumulative'
-        :param scale: 'million'
-        :return: int
+        :param county: str, e.g. 'Cook'
+        :param scale: int
+        :param span: int, e.g. 1 for today, 7 for this week
+        :return: float
         """
-        raise NotImplementedError
+        population = self.get_population(state=state, county=county)
+        confirmed_cases = self.confirmed_cases(date=date, state=state, county=county, span=span)
+
+        return round(confirmed_cases / population * scale, 1)
 
     def cure_rate(self, date, state=None, county=None, span=1):
         """
@@ -166,10 +173,63 @@ class Covid19(object):
         """
         raise NotImplementedError
 
-    def most_confirmed_cases_per_capita(self, state=None):
+    def most_confirmed_cases_per_capita(self, date, state=None, scale=100000, span=999, type='most'):
         """
         The state/county with the most confirmed cases per capita
+        :param date: str, e.g. '2020-04-23'
         :param state: str, e.g. 'Illinois'
+        :param scale: int
+        :param span: int, e.g. 1 for today, 7 for this week
+        :param type: str, 'most' or 'least'
         :return: str
         """
-        raise NotImplementedError
+        df = self.df_states.copy()
+        if state:
+            df = self.df_counties.copy()
+            df = df[df['state'] == state]
+        df = df.reset_index(drop=True)
+
+        if state:
+            candidates = df['county'].unique()
+            cases_per_capita = {county: self.confirmed_case_per_capita(date, state=state, county=county, scale=scale, span=span) for county in candidates}
+        else:
+            candidates = df['state'].unique()
+            cases_per_capita = {state: self.confirmed_case_per_capita(date, state=state, scale=scale, span=span) for state in candidates}
+
+        if type == 'most':
+            result = max(zip(cases_per_capita.values(), cases_per_capita.keys()))
+        else:
+            non_zeros = {k: v for k, v in cases_per_capita.items() if v != 0.0}
+            result = min(zip(non_zeros.values(), non_zeros.keys()))
+        return result
+
+    def get_population(self, state=None, county=None):
+        """
+        Get population of a specific state/county
+        :param state: str, e.g. 'Illinois'
+        :param county: str, e.g. 'Cook'
+        :return: int
+        """
+        if state and county:
+            df = self.df_counties.copy()
+            df = df[(df['state'] == state) & (df['county'] == county)]
+        elif state:
+            df = self.df_states.copy()
+            df = df[df['state'] == state]
+        else:
+            df = self.df_us.copy()
+        df = df.reset_index(drop=True)
+        fips = df['fips'][0] if state else None
+
+        params = {
+            'get': 'POP',
+            'for': 'county:{}'.format(fips[2:]) if county else 'state:{}'.format(fips) if state else 'us:*',
+            'in': 'state:{}'.format(fips[0:2]) if county else None,
+            'key': API_KEY
+        }
+        response = requests.get(url=BASE_URL, params=params)
+        if response.status_code != 200:
+            import sys
+            return sys.maxsize * 2 + 1
+        data = response.json()
+        return int(data[1][0])
